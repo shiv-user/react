0ff0017af72722eddb657f537c73de0868bd3cff
shiv-user <shivranjan1497@gmail.com>
2020-05-12 23:24:56 +0530

    newfiles

---
+++ five-themeswitcher/src/Color.js
@@ -0,0 +1,14 @@
+const AppTheme = {
+    light:{
+        textColor:"#000",
+        backgroundColor:"#FFF"
+
+    },
+    dark:{
+        textColor:"#fff",
+        backgroundColor:"#333"
+
+    }
+
+}
+export default AppTheme;
